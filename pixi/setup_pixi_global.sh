d8fc4af31381aeedd64d3d2e8af6b2200d04e199
Yuanyuan Wei <aywi@users.noreply.github.com>
2026-03-02 22:25:14 +0000

    Update setup_pixi_global.sh

--- pixi/setup_pixi_global.sh
+++ pixi/setup_pixi_global.sh
@@ -8,7 +8,7 @@ pixi g i -e tools pip python=3.13
 pixi g a -e tools byobu --expose byobu
 pixi g a -e tools cmake --expose ccmake --expose cmake --expose cpack --expose ctest
 pixi g a -e tools conda conda-recipe-manager conda-smithy --expose conda
-pixi g a -e tools git --expose git --expose gitk
+pixi g a -e tools git git-lfs --expose git --expose gitk
 pixi g a -e tools gojq --expose gojq
 pixi g a -e tools htop --expose htop
 pixi g a -e tools jaq --expose jaq
@@ -17,6 +17,6 @@ pixi g a -e tools mamba --expose mamba
 pixi g a -e tools micromamba --expose micromamba
 pixi g a -e tools nvitop --expose nvisel --expose nvitop
 pixi g a -e tools nvtop --expose nvtop
-pixi g a -e tools tmux --expose tmux
+pixi g a -e tools tmux!=3.6 --expose tmux
 pixi g a -e tools uv --expose uv --expose uvx
 pixi g ls
